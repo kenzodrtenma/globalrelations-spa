86e9e2b87945fc1fb53551f4ff4290c9b7320cdd
Petrus Nogueira <petrus.wejam@gmail.com>
2023-11-18 14:22:06 -0300

    feat: country selector done

--- src/App.vue
+++ src/App.vue
@@ -19,20 +19,27 @@ export default {
   margin: 0;
 }
 
+body {
+  min-height: 100vh;
+  background-color: #f4f4f4;
+}
+
 #app {
   font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  margin-bottom: 30px;
-  padding: 0 20px;
+  padding-top: 60px;
+  padding-bottom: 30px;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
 @media screen and (min-width: 768px) {
   #app {
-    padding: 0;
+    padding-left: 0;
+    padding-right: 0;
   }
 }
 </style>
--- src/components/CountriesSelector.vue
+++ src/components/CountriesSelector.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="countries-selector">
+    <div class="world-loading" v-if="loading">
+      <div class="world-loading-image">
+        <img src="../assets/world-loading.gif" alt="image of a earth globe spinning">
+      </div>
+      <span>Carregando países...</span>
+    </div>
+    <div  v-if="!loading && data" class="countries-selector">
       <div class="selectors">
         <div class="selector">
           <div class="flag">
-            <img src="https://flagcdn.com/br.svg" alt="">
+            <div v-if="first_flag_loading" class="world-loading-image">
+              <img src="../assets/world-loading.gif" alt="image of a earth globe spinning">
+            </div>
+            <img v-if="!first_flag_loading" :src="first_country_flag" alt="">
           </div>
-          <select>
+          <select v-model="first_country" @change="handleCountrySelect(1)">
+            <option value="Select the first country">Select the first country</option>
             <option v-for="country in countries" :key="country.name" :value="country.name">
-              {{ country.name }}
+              {{ country.name.common }}
             </option>
           </select>
         </div>
         <p>and</p>
         <div class="selector">
           <div class="flag">
-            <img src="https://flagcdn.com/br.svg" alt="">
+            <div v-if="second_flag_loading" class="world-loading-image">
+              <img src="../assets/world-loading.gif" alt="image of a earth globe spinning">
+            </div>
+            <img v-if="!second_flag_loading" :src="second_country_flag" alt="">
           </div>
-          <select>
-            <option v-for="country in countries" :key="country.name" :value="country.name">
-              {{ country.name }}
+          <select v-model="second_country" @change="handleCountrySelect(2)">
+            <option value="Select the second country">Select the second country</option>
+            <option v-for="country in countries" :key="country.name" :value="country.name">              
+              {{ country.name.common }}
             </option>
           </select>
         </div>
@@ -28,32 +42,57 @@
 </template>
   
 <script>
+  import axios from 'axios';
+
   export default {
     data() {
       return {
-        countries: []
+        data: [],
+        countries: [],
+        first_country_flag: "../assets/un.png",
+        second_country_flag: "../assets/un.png",
+        first_flag_loading: false,
+        second_flag_loading: false,
+        loading: true
       };
     },
 
     methods: {
       getCountries() {
-        this.countries = [
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'},
-          {'name': 'Brazil'}
-        ]
+        this.loading = true;
+
+        axios.get('https://restcountries.com/v3.1/independent?status=true')
+        .then(response => {
+          this.loading = false
+          this.countries = response.data
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.error("Erro na requisição:", error)
+          this.loading = false
+        })
+      },
+
+      handleCountrySelect(country_number) {
+        
+        if (country_number == 1) {
+          this.first_flag_loading = true;
+          let selected_country = this.countries.find(country => country.name == this.first_country)
+          this.first_country_flag = selected_country.flags.svg
+
+          setTimeout(() => {
+            this.first_flag_loading = false
+          }, 500);
+          
+        } else if (country_number == 2) {
+          this.second_flag_loading = true;
+          let selected_country = this.countries.find(country => country.name == this.second_country)
+          this.second_country_flag = selected_country.flags.svg
+
+          setTimeout(() => {
+            this.second_flag_loading = false
+          }, 500);
+        }
       }
     },
 
@@ -64,6 +103,22 @@
 </script>
 
 <style>
+
+  .world-loading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .world-loading-image {
+    width: 150px;
+    height: 150px;
+    margin-bottom: 10px;
+  }
+
+  .world-loading-image img {
+    width: 100%;
+    height: 100%;
+  }
 
   .countries-selector .selectors {
     display: flex;
@@ -78,6 +133,10 @@
     width: 100%;
   }
 
+  .countries-selector select option {
+    height: 10px;
+  }
+
   .countries-selector select,
   .countries-selector .submit {
     height: 50px;
@@ -90,8 +149,6 @@
   }
 
   .countries-selector .flag {
-    width: 320px;
-    height: 220px;
     display: none;
   }
 
@@ -117,7 +174,15 @@
 
   @media screen and (min-width: 768px) {
     .countries-selector .flag {
-      display: block;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 320px;
+      height: 220px;
+      margin-bottom: 30px;
+      border: 2px solid #999;
+      border-radius: 10px;
+      overflow: hidden;
     }
 
     .countries-selector .selector,
